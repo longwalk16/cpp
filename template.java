6b6002f04ba8aad73d00996577f9f7b1d6f41209
longwalk16 <95503486+longwalk16@users.noreply.github.com>
2021-12-03 15:08:16 -0600

    Create template.java

---
+++ template.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+ 
+public class A {
+ 
+	public static void main(String[] args) {
+		FastScanner fs = new FastScanner();
+		PrintWriter pw = new PrintWriter(System.out);
+		StringBuilder sb = new StringBuilder("");
+    int t = fs.nextInt();
+    for (int tt = 0; tt < t; tt++) {
+      
+    }
+    pw.print(sb.toString());
+    pw.close();
+ 
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		String next() {while (!st.hasMoreTokens())try {st = new StringTokenizer(br.readLine());} catch (IOException e) {}return st.nextToken();}
+		int nextInt() {return Integer.parseInt(next());}
+		long nextLong() {return Long.parseLong(next());}
+		double nextDouble() {return Double.parseDouble(next());}
+		long[] readArray(int n) { long[] a = new long[MAX]; for (int i = 1; i <= n; i++) {a[i] = nextLong();} return a;}
+	}
