a32f8a979ae317ac9020cd6c2d0ce48ea75cc378
longwalk16 <95503486+longwalk16@users.noreply.github.com>
2021-12-03 15:15:32 -0600

    Create ternarySearch.java

---
+++ searching_sorting/ternarySearch.java
@@ -0,0 +1,17 @@
+static double ternarySearch(int times) {
+while (times >= 0) {
+				m1 = l + (r - l) / 3;
+				m2 = r - (r - l) / 3;
+
+				double dist1 = dist(m1);
+				double dist2 = dist(m2);
+
+				if (dist1 < dist2) {
+					r = m2;
+				} else {
+					l = m1;
+				}
+				times--;
+			}
+  return dist(l);
+}
