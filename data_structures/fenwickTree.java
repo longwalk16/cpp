b7dd4ad4304514e0b31ae7e734e6035a3816f0bb
longwalk16 <95503486+longwalk16@users.noreply.github.com>
2021-12-03 14:21:22 -0600

    Update fenwickTree.java

--- data_structures/fenwickTree.java
+++ data_structures/fenwickTree.java
@@ -1,1 +1,21 @@
+static final int MAX = (int) 2e5 + 7;
+static long[] a = new long[MAX];
+static long[] b = new long[MAX];
+static long[] tree = new long[MAX];
+static long ans;
 
+static long query(int x) {
+		int ans = 0;
+		while (x > 0) {
+			ans += tree[x];
+			x -= (x & -x);
+		}
+		return ans;
+	}
+	
+static void update(int x, long val) {
+		while (x <= n) {
+			tree[x] += val;
+			x += (x & -x);
+		}
+	}
